01eb7909ae7299116eaf9a7dee70e3470d464e36
huyikun <48144200+huyikun@users.noreply.github.com>
2019-08-25 17:09:05 +0800

    submit

--- src/views/HouseList.vue
+++ src/views/HouseList.vue
@@ -5,11 +5,11 @@
         <v-col
           v-for="(house, index) in houseList"
           :key="index"
-          v-if="index >= (page - 1) * 3 && index < page * 3"
+          v-if="index >= (page - 1) * cnt && index < page * cnt"
         >
-          <v-card>
+          <v-card style="max-width: 300px;">
             <v-img
-              :src="house.photoList[0]"
+              :src="require('@/assets/pic2.jpg')"
               class="white--text"
               height="200px"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
@@ -21,8 +21,43 @@
             </v-img>
 
             <v-card-actions>
-              <div class="flex-grow-1"></div>
-              <v-btn @click>DETAILS</v-btn>
+              <v-card-text>￥{{ house.price }} / 晚</v-card-text>
+              <v-spacer />
+              <v-dialog v-model="dialog[index % cnt]" width="500">
+                <template v-slot:activator="{ on }">
+                  <v-btn color="red lighten-2" dark v-on="on">
+                    Details
+                  </v-btn>
+                </template>
+
+                <v-card>
+                  <v-card-title class="headline grey lighten-1" primary-title>
+                    欢迎您预览 {{ house.name }}
+                  </v-card-title>
+                  <v-carousel
+                    :continuous="false"
+                    :cycle="cycle"
+                    hide-delimiter-background
+                    height="250"
+                  >
+                    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                      <v-img
+                        :src="require('@/assets/pic2.jpg')"
+                        height="200px"
+                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      />
+                    </v-carousel-item>
+                  </v-carousel>
+                  <v-card-text> {{ house.introduce }} </v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <div class="flex-grow-1"></div>
+                    <v-btn color="primary" text @click="closeall">
+                      I accept
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -32,7 +67,7 @@
       <v-pagination
         total-visible="5"
         v-model="page"
-        :length="houseList.length / 3"
+        :length="Math.ceil(houseList.length / cnt)"
       ></v-pagination>
     </div>
   </div>
@@ -41,56 +76,82 @@
 export default {
   data: () => ({
     page: 1,
+    cnt: 3,
+    dialog: [
+      false,
+      false,
+      false,
+    ],
+    cycle: false,
+    slides: [
+      'First',
+      'Second',
+      'Third',
+      'Fourth',
+      'Fifth',
+    ],
     houseList: [
       {
-        address: "1",
+        name: "room1",
+        address: "address1",
+        price: 10.0,
         size: "",
-        introduce: "",
+        introduce: "iam1",
         rent: 0.0,
         houseOwnerPhone: "",
-        photoList: ["src/assets/img/2B 01.jpg",],
+        photoList: ["../assets/pic2.jpg",],
         timeLine: []
       },
       {
-        address: "2",
+        name: "room2",
+        address: "address2",
+        price: 20.0,
         size: "",
-        introduce: "",
+        introduce: "iam2",
         rent: 0.0,
         houseOwnerPhone: "",
         photoList: ["src/assets/img/2B 02.jpg"],
         timeLine: []
       },
       {
-        address: "3",
+        name: "room3",
+        address: "address3",
+        price: 30.0,
         size: "",
-        introduce: "",
+        introduce: "iam3",
         rent: 0.0,
         houseOwnerPhone: "",
         photoList: ["../assets/img/Andersen.jpg"],
         timeLine: []
       },
       {
-        address: "4",
+        name: "room4",
+        address: "address4",
+        price: 40.0,
         size: "",
-        introduce: "",
+        introduce: "iam4",
         rent: 0.0,
         houseOwnerPhone: "",
         photoList: ["../assets/img/Angela.jpg"],
         timeLine: []
       },
       {
-        address: "5",
+        name: "room5",
+        address: "address5",
+        price: 50.0,
         size: "",
-        introduce: "",
+        introduce: "iam5",
         rent: 0.0,
         houseOwnerPhone: "",
         photoList: ["../assets/img/Cuora.jpg"],
         timeLine: []
       },
       {
-        address: "6",
+        name: "room6",
+        address: "address6",
+        price: 60.0,
         size: "",
-        introduce: "",
+        introduce: "iam6",
         rent: 0.0,
         houseOwnerPhone: "",
         photoList: ["../assets/img/Date A Live.jpg"],
@@ -98,5 +159,12 @@ export default {
       }
     ]
   }),
+  methods: {
+    closeall () {
+      this.dialog[0] = false
+      this.dialog[1] = false
+      this.dialog[2] = false
+    }
+  }
 };
 </script>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,18 +1,92 @@
 <template>
-    <div id="div1">
-        
+  <div class="form-group">
+    <label class="control-label">上传图片</label>
+    <div class="control-form">
+      <p class="help-block">
+        (建议图片格式为：JPEG/BMP/PNG/GIF，大小不超过5M，最多可上传4张)
+      </p>
+      <ul class="upload-imgs">
+        <li v-if="imgLen >= 4 ? false : true">
+          <input
+            type="file"
+            class="upload"
+            @change="addImg"
+            ref="inputer"
+            multiple
+            accept="image/png,image/jpeg,image/gif,image/jpg"
+          />
+          <v-btn @click="submit">点击上传</v-btn>
+        </li>
+        <li v-for="(value, key) in imgs">
+          <p class="img">
+            <img :src="getObjectURL(value)" /><a
+              class="close"
+              @click="delImg(key)"
+              >×</a
+            >
+          </p>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    
+  data () {
+    return {
+      formData: new FormData(),
+      imgs: {},
+      imgLen: 0,
+    }
+  },
+  methods: {
+    addImg (event) {
+      let inputDOM = this.$refs.inputer;
+      // 通过DOM取文件数据
+      this.fil = inputDOM.files;
+      let oldLen = this.imgLen;
+      let len = this.fil.length + oldLen;
+      if (len > 4) {
+        alert('最多可上传4张，您还可以上传' + (4 - oldLen) + '张');
+        return false;
+      }
+      for (let i = 0; i < this.fil.length; i++) {
+        let size = Math.floor(this.fil[i].size / 1024);
+        if (size > 5 * 1024 * 1024) {
+          alert('请选择5M以内的图片！');
+          return false
+        }
+        this.imgLen++;
+        this.$set(this.imgs, this.fil[i].name + '?' + new Date().getTime() + i, this.fil[i]);
+      }
+    },
+    getObjectURL (file) {
+      var url = null;
+      if (window.createObjectURL != undefined) { // basic
+        url = window.createObjectURL(file);
+      } else if (window.URL != undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file);
+      } else if (window.webkitURL != undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file);
+      }
+      return url;
+    },
+    delImg (key) {
+      this.$delete(this.imgs, key);
+      this.imgLen--;
+    },
+    submit () {
+      for (let key in this.imgs) {
+        console.log(this.imgs[key])
+        let name = key.split('?')[0];
+        this.formData.append('multipartFiles', this.imgs[key], name);
+      }
+      this.$axios.post('/opinion', this.formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }).then(res => {
+        this.alertShow = true;
+      });
+    },
+  }
 }
 </script>
-<style>
-#div1{
-    width: 200px;
-    height: 200px;
-    border: 1px solid #000;
-    background-color: aqua;
-}
-</style>
