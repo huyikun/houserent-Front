bae5d2d1c4f7e2d96bb7adfc758506d0b51246cc
huyikun <48144200+huyikun@users.noreply.github.com>
2019-09-01 02:13:05 +0800

    last version2

--- src/components/ShortRentOrder.vue
+++ src/components/ShortRentOrder.vue
@@ -7,7 +7,17 @@
         订单明细
       </v-card-title>
       <v-divider />
-      <v-card-text style="text-align: left"> </v-card-text>
+      <v-card-text style="text-align: left">
+        承租方： {{ this.order.userName }} <br />
+        出租方： {{ this.order.ownerName }} <br />
+        房屋名称： {{ this.order.houseName }} <br />
+        房屋地址： {{ this.order.address }} <br />
+        入住日期： {{ this.order.checkinDate }} <br />
+        退房日期： {{ this.order.checkoutDate }} <br />
+        预订天数： {{ this.order.rentDays }} <br />
+        支付资金： {{ this.order.totalRent }} <br />
+      </v-card-text>
+      <v-img :src="require('@/../static/img/imgforpay.jpg')">
       <v-card-actions>
         <v-spacer />
         <v-btn color="primary">OK</v-btn>
@@ -18,7 +28,11 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      order: this.$store.state.order,
+    }
+  }
 }
 </script>
 
